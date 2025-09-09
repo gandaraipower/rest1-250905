d8c8054df3aa8dff7f0290912e9c2a66cb9a8357
gandaraipower <jjangjongsoo@naver.com>
2025-09-09 14:17:29 +0900

    30강

--- src/main/java/com/rest1/domain/post/comment/controller/ApiV1CommentController.java
+++ src/main/java/com/rest1/domain/post/comment/controller/ApiV1CommentController.java
@@ -5,12 +5,12 @@ import com.rest1.domain.post.comment.entity.Comment;
 import com.rest1.domain.post.post.entity.Post;
 import com.rest1.domain.post.post.service.PostService;
 import com.rest1.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,7 +42,7 @@ public class ApiV1CommentController {
         return new CommentDto(comment);
     }
 
-    @GetMapping("/{postId}/comments/{commentId}/delete")
+    @DeleteMapping("/{postId}/comments/{commentId}")
     @Transactional
     public RsData<Void> deleteItem(
             @PathVariable Long postId,
@@ -50,16 +50,46 @@ public class ApiV1CommentController {
     ) {
 
         Post post = postService.findById(postId).get();
-        Comment comment = post.findCommentById(commentId).get();
 
         postService.deleteComment(post, commentId);
 
-        RsData<Void> rsData = new RsData<>(
-                "204-1",
+        return new RsData<>(
+                "200-1",
                 "%d번 댓글이 삭제되었습니다.".formatted(commentId)
         );
+    }
 
-        return rsData;
+
+    record CommentWriteReqBody(
+
+            @NotBlank
+            @Size(min = 2, max = 100)
+            String content
+    ) {
+    }
+
+    record CommentWriteResBody(
+            CommentDto CommentDto
+    ) {}
+
+    @PostMapping("/{postId}/comments")
+    @Transactional
+    public RsData<CommentWriteResBody> createItem(
+            @PathVariable Long postId,
+            @RequestBody @Valid CommentWriteReqBody reqBody
+    ) {
+        Post post = postService.findById(postId).get();
+        Comment comment=postService.writeComment(post, reqBody.content);
+
+        postService.flush();
+
+        return new RsData<>(
+                "201-1",
+                "%d번 댓글이 생성되었습니다.".formatted(post.getId()),
+                new CommentWriteResBody(
+                        new CommentDto(comment)
+                )
+        );
     }
 
 
